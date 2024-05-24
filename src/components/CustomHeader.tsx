eb667a81c09d78e31177b27be513dd9858b656e5
RafaelPil <rafael.pilumyan@gmail.com>
2024-05-24 20:16:37 +0300

    Update: Custom Header

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {View, Image, Pressable} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
+import FocusedStatusBar from './CustomStatusBar';
 
 const CustomHeader = () => {
   const navigation = useNavigation();
 
   return (
-    <View className="flex-row items-center justify-between p-4 bg-slate-800">
-      <View className="flex-row items-center">
-        <TouchableOpacity onPress={() => navigation.openDrawer()}>
-          <Ionicons name="menu" size={24} color="black" />
-        </TouchableOpacity>
-        <Text className="ml-4 text-lg font-bold">Header Title</Text>
-      </View>
-      <Image
-        source={{uri: 'https://example.com/your-image.jpg'}}
-        className="w-10 h-10 rounded-full"
+    <View className="flex-row items-center justify-between p-4 bg-white">
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="#fff"
+        transLucent={true}
       />
+      <View className="flex-row">
+        <Pressable
+          onPress={() => navigation.getParent('LeftDrawer').openDrawer()}>
+          <Feather name="menu" size={24} color="black" />
+        </Pressable>
+        <Image
+          source={require('../assets/img/Reddit_Logo.png')}
+          className="w-20 h-5 ml-2"
+          resizeMode="contain"
+        />
+      </View>
+      <View className="flex-row items-center">
+        <AntDesign name="search1" size={24} color="black" />
+        <Pressable
+          onPress={() => navigation.getParent('RightDrawer').openDrawer()}>
+          <Image
+            source={{
+              uri: 'https://www.redditstatic.com/avatars/defaults/v2/avatar_default_6.png',
+            }}
+            className="w-8 h-8 rounded-full ml-6"
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
